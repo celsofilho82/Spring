6746f205c1d8dc3d7590467dba3eefbf22596f0a
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-10 21:53:24 -0300

    Usando @Configuration e @Bean para definir beans

---
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/AlgaConfig.java
@@ -0,0 +1,10 @@
+package com.algaworks.algafood;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AlgaConfig {
+
+	// Utilizaremos essa classe para servir como definição dos Beans 
+	
+}
