3e73a2290921e7fbe273750576f2d16a19d7b65f
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-11 21:29:18 -0300

    Dependência opcional com Autowired

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -7,32 +7,20 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 
 public class AtivacaoClienteService {
 
-	// Podemos fazer a injeção de dependência com a notação @Autowired nos seguintes pontos:
-	// No construtor da classe
-	// No atributo da classe
-	// Nos método setters da classe
+	// Podemos configurar uma dependência opcional no projeto passando o parâmetro
+	// (required = false) para a anotação @Autowired
 
-	@Autowired
+	@Autowired(required = false)
 	private Notificador notificador;
-
-//	@Autowired
-//	public AtivacaoClienteService(Notificador notificador) {
-//		this.notificador = notificador;
-//	}
-//	
-//	public AtivacaoClienteService(String qualquer) {
-//		
-//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		if (notificador != null) {
+			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		} else {
+			System.out.println("Não existe notificador, mas cliente foi ativado");
+		}
 	}
-
-//	@Autowired
-//	public void setNotificador(Notificador notificador) {
-//		this.notificador = notificador;
-//	}
 
 }
