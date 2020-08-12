b77347f3aff9a0e4f414e1f818356edbb0640029
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-11 21:42:03 -0300

    Ambiguidade de beans e injeção de lista de beans

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.di.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -7,19 +9,19 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 
 public class AtivacaoClienteService {
 
-	// Podemos configurar uma dependência opcional no projeto passando o parâmetro
-	// (required = false) para a anotação @Autowired
+	// Neste exemplo temos uma interface Notificacao com duas implementações
+	// NotificacaoSMS e NotificacaoEmail. Para resolver o problema da ambiquidade,
+	// podemos passar para o spring uma lista de implementações de notificação e
+	// fazer o uso para gerar uma notificação para cada implementação
 
-	@Autowired(required = false)
-	private Notificador notificador;
+	@Autowired
+	private List<Notificador> notificadores;
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		if (notificador != null) {
+		for (Notificador notificador : notificadores) {
 			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-		} else {
-			System.out.println("Não existe notificador, mas cliente foi ativado");
 		}
 	}
 
