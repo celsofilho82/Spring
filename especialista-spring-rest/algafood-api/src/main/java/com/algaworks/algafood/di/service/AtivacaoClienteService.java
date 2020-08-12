2074889453513152f99dca0ed17dc9cea1f7dc4a
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-11 21:22:05 -0300

    Conhecendo os pontos de injeção e a anotação Autowired

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,22 +1,38 @@
 package com.algaworks.algafood.di.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 import com.algaworks.algafood.di.notificacao.Notificador;
 
 public class AtivacaoClienteService {
 
+	// Podemos fazer a injeção de dependência com a notação @Autowired nos seguintes pontos:
+	// No construtor da classe
+	// No atributo da classe
+	// Nos método setters da classe
+
+	@Autowired
 	private Notificador notificador;
-	
-	public AtivacaoClienteService(Notificador notificador) {
-		this.notificador = notificador;
-		
-		System.out.println("AtivacaoClienteService: " + notificador);
-	}
+
+//	@Autowired
+//	public AtivacaoClienteService(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+//	
+//	public AtivacaoClienteService(String qualquer) {
+//		
+//	}
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
 		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
-	
+
+//	@Autowired
+//	public void setNotificador(Notificador notificador) {
+//		this.notificador = notificador;
+//	}
+
 }
