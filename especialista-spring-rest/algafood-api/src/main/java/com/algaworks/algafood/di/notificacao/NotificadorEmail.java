a0c24e3a4c8ff37812674058892c4a49702d12d3
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-11 21:48:35 -0300

    Desambiguação de beans com Primary

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,30 +1,20 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
 import com.algaworks.algafood.di.modelo.Cliente;
 
-public class NotificadorEmail implements Notificador{
+@Primary
+@Component
+public class NotificadorEmail implements Notificador {
 
-	private boolean caixaAlta;
-	private String hostServidorSmtp;
-	
-	public NotificadorEmail(String hostServidorSmpt) {
-		this.hostServidorSmtp = hostServidorSmpt;
-		System.out.println("NotificadorEmail");
-	}
-	
+	// Usando a anotação @Primary para resolver conflitos de ambiguidade em uma
+	// classe do mesmo tipo
+
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		
-		if(this.caixaAlta) {
-			mensagem = mensagem.toUpperCase();
-		}
-		
-		System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", 
-				cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, mensagem);
+		System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), mensagem);
 	}
 
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
-	
 }
--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.di.service;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -9,20 +7,13 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 
 public class AtivacaoClienteService {
 
-	// Neste exemplo temos uma interface Notificacao com duas implementações
-	// NotificacaoSMS e NotificacaoEmail. Para resolver o problema da ambiquidade,
-	// podemos passar para o spring uma lista de implementações de notificação e
-	// fazer o uso para gerar uma notificação para cada implementação
-
 	@Autowired
-	private List<Notificador> notificadores;
-
+	private Notificador notificador;
+	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		for (Notificador notificador : notificadores) {
-			notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-		}
+		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
 	}
 
 }
