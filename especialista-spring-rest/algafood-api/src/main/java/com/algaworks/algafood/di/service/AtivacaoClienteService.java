e43c8a2011720bc0118f722ae3c21377d92716cf
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-12 22:02:11 -0300

    Publicando e consumindo eventos customizados

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,40 +1,24 @@
 package com.algaworks.algafood.di.service;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 
 @Component
 public class AtivacaoClienteService {
-	
-	//Usando a anotação customizada para definir um qualificador para a classe
-	@TipoDoNotificador(NivelUrgencia.NORMAL)
+
 	@Autowired
-	private Notificador notificador;
-	
-	// Esse método será executado quando o bean for carregado
-	@PostConstruct
-	public void init() {
-		System.out.println("INIT");
-	}
-	
-	// Esse método será executado antes que o bean seja destruido
-	@PreDestroy
-	public void destroy() {
-		System.out.println("DESTROY");
-	}
+	private ApplicationEventPublisher eventPublisher;
 
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
 
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+		// Usando o padrão de projetos observer para notificar a classe
+		// ClienteAtivadoEvent que um temos um evento de ativação de usuário
+		eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
+
 	}
 
 }
