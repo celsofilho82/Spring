aec3c1af7d4d8d81716e16fd9fffce6bc0c8b69f
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-12 21:20:02 -0300

    Mudando o comportamento da aplicação com Spring Profiles

---
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmailMock.java
@@ -0,0 +1,26 @@
+package com.algaworks.algafood.di.notificacao;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+
+// Usamos a anotação @Profile para especificar em qual ambiente devemos carregar essa classe
+// No arquivo aplication.properties devemos informar qual ambiente estamos através desta declaração: spring.profiles.active={ambiente}
+
+@Profile("dev")
+@TipoDoNotificador(NivelUrgencia.NORMAL)
+@Component
+public class NotificadorEmailMock implements Notificador {
+
+	public NotificadorEmailMock() {
+		System.out.println("NotificadorEmail MOCK");
+	}
+
+	@Override
+	public void notificar(Cliente cliente, String mensagem) {
+		System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(),
+				cliente.getEmail(), mensagem);
+	}
+
+}
