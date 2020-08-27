fd35bd2b6a6cfd106728eb78b2513fda6c909986
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-26 22:06:58 -0300

    Modelando e implementando a inclusão de recursos com POST

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -6,8 +6,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,7 +21,6 @@ public class Cozinha {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	@JsonProperty("titulo")
 	@Column(nullable = false)
 	private String nome;
 
