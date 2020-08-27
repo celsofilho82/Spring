c0c0a0d00523b2c9b8460e4caf482f0e4079b73e
Celso Ribeiro <celsofilho82@gmail.com>
2020-08-26 22:33:21 -0300

    Modelando e implementando a atualização de recursos com PUT

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -29,27 +30,41 @@ public class CozinhaController {
 	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
 	}
-	
+
 	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
 	public CozinhasXmlWrapper listarXml() {
 		return new CozinhasXmlWrapper(cozinhaRepository.listar());
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
 		Cozinha cozinha = cozinhaRepository.buscar(id);
-		
+
 		if (cozinha != null) {
 			// return ResponseEntity.status(HttpStatus.OK).body(cozinha);
-			return ResponseEntity.ok(cozinha);			
+			return ResponseEntity.ok(cozinha);
 		}
 		// return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.salvar(cozinha);
 	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long id, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.buscar(id);
+
+		if (cozinhaAtual != null) {
+			cozinhaAtual.setNome(cozinha.getNome());
+			Cozinha cozinhaAtualizada = cozinhaRepository.salvar(cozinhaAtual);
+			return ResponseEntity.ok(cozinhaAtualizada);
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
+
 }
