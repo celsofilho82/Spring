b12244cc26f6757ac3cdbf61bc3c3ffbf387170b
Celso Ribeiro <celsofilho82@gmail.com>
2020-09-10 21:55:48 -0300

    Criando uma fábrica de Specifications

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/teste/TesteController.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/teste/TesteController.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.teste;
 
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comFreteGratis;
+import static com.algaworks.algafood.infrastructure.repository.spec.RestauranteSpecs.comNomeSemelhante;
+
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
@@ -16,8 +19,6 @@ import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
-import com.algaworks.algafood.infrastructure.repository.spec.RestauranteComFreteGratisSpec;
-import com.algaworks.algafood.infrastructure.repository.spec.RestauranteComNomeSemelhanteSpec;
 
 // Controller de teste para demonstrar a implementação consultas JPQL em repositórios
 
@@ -83,9 +84,8 @@ public class TesteController {
 	
 	@GetMapping("/restaurantes/com-frete-gratis")
 	public List<Restaurante> restaurantesComFreteGratis(String nome) {
-		var comFreteGratis = new RestauranteComFreteGratisSpec();
-		var comNomeSemelhante = new RestauranteComNomeSemelhanteSpec(nome);
 		
-		return restauranteRepository.findAll(comFreteGratis.and(comNomeSemelhante));
+		return restauranteRepository.findAll(comFreteGratis()
+				.and(comNomeSemelhante(nome)));
 	}
 }
