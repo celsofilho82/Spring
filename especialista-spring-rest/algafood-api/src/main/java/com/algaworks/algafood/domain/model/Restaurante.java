6b60a8430eb242d8891d5a4b843f65db6f9b3ec5
Celso Ribeiro <celsofilho82@gmail.com>
2020-09-15 22:00:47 -0300

    Mapeando relacionamento muitos-para-muitos com ManyToMany

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -1,13 +1,20 @@
 package com.algaworks.algafood.domain.model;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.ManyToAny;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -32,4 +39,10 @@ public class Restaurante {
 	
 	@ManyToOne
 	private Cozinha cozinha;
+	
+	@ManyToMany
+	@JoinTable(name = "restaurante_forma_pagamento", 
+	joinColumns = @JoinColumn(name = "restaurante_id"),
+	inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+	private List<FormaPagamento> formasPagamento = new ArrayList<>();
 }
