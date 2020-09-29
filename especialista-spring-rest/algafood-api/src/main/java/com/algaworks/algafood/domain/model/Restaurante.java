25d58dffe6329c7c259265d57277e8c43ad68798
Celso Ribeiro <celsofilho82@gmail.com>
2020-09-28 22:04:05 -0300

    Alterando a estratégia de fetching para Lazy Loading

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,6 +22,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -57,7 +59,8 @@ public class Restaurante {
 	@Column(nullable = false, columnDefinition = "datetime")
 	private LocalDateTime dataAtualizacao;
 	
-	@ManyToOne
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonIgnoreProperties("hibernateLazyInitializer")
 	private Cozinha cozinha;
 	
 	@JsonIgnore
