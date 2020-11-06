b1469f0fb9655e31e2fa5f2de8a575c270cbb2ce
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-05 22:46:18 -0300

    Desafio: lançando exceptions de granularidade fina

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/RestauranteNaoEncontradoException.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/RestauranteNaoEncontradoException.java
@@ -7,11 +7,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Podemos utilizar a anotação @ResponseStatus para fazer com que uma exception quando lançada retorne um status HTTP
  */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
-public abstract class EntidadeNaoEncontradaException extends NegocioException{
+public class RestauranteNaoEncontradoException extends EntidadeNaoEncontradaException {
 
 	private static final long serialVersionUID = 1L;
 
-	public EntidadeNaoEncontradaException(String mensagem) {
+	public RestauranteNaoEncontradoException(String mensagem) {
 		super(mensagem);
+	}
+	
+	public RestauranteNaoEncontradoException(Long cidadeId) {
+		this(String.format("Não existe um Estado com esse código %d", cidadeId));
 	}
 }
