1f1cf11954412c5de4d531dca157a0c7f63ed07d
Celso Ribeiro <celsofilho82@gmail.com>
2020-11-09 21:29:19 -0300

    Desafio: implementando exception handler

--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeEmUsoException.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/EntidadeEmUsoException.java
@@ -1,13 +1,10 @@
 package com.algaworks.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /*
  * Podemos utilizar a anotação @ResponseStatus para fazer com que uma exception quando lançada retorne um status HTTP
  */
-@ResponseStatus(value = HttpStatus.CONFLICT)
-public class EntidadeEmUsoException extends RuntimeException {
+
+public class EntidadeEmUsoException extends NegocioException {
 
 	private static final long serialVersionUID = 1L;
 
--- especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
+++ especialista-spring-rest/algafood-api/src/main/java/com/algaworks/algafood/domain/exception/NegocioException.java
@@ -1,12 +1,9 @@
 package com.algaworks.algafood.domain.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 /*
  * Podemos utilizar a anotação @ResponseStatus para fazer com que uma exception quando lançada retorne um status HTTP
  */
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+
 public class NegocioException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
