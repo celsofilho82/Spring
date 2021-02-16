114246618aa8a734031ec31786b39884f899dcd4
Celso Ribeiro <celsofilho82@gmail.com>
2021-02-16 16:41:47 -0300

    Alterando projeto para gerar um WAR no build

--- spring-boot-parte03/projeto-inicial/src/main/java/br/com/alura/forum/ForumApplication.java
+++ spring-boot-parte03/projeto-inicial/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,6 +2,8 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -11,10 +13,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication {
+public class ForumApplication extends SpringBootServletInitializer {
+
+	/*
+	 * Para gerar um WAR da aplicação para ser executado em um sevlet container
+	 * externo(ex: tomcat) devemos fazer as seguintes modificações: 
+	 * Alteração no arquivo pom.xml 
+	 * Aplicar herança na classe principal do projeto herdando da
+	 * classe SpringBootServletInitializer
+	 * Sobrescrever o método SpringApplicationBuilder que vai retornar a classe principal
+	 * ForumApplication.class
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ForumApplication.class);
 	}
 
 }
