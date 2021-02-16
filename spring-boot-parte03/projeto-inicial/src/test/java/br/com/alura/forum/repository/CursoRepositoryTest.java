b32e15a2d7424765b711c3d4dccfae0cc32c40d7
Celso Ribeiro <celsofilho82@gmail.com>
2021-02-16 12:30:04 -0300

    Configurando um banco de dados de teste

--- spring-boot-parte03/projeto-inicial/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
+++ spring-boot-parte03/projeto-inicial/src/test/java/br/com/alura/forum/repository/CursoRepositoryTest.java
@@ -5,7 +5,11 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.com.alura.forum.modelo.Curso;
@@ -13,10 +17,14 @@ import br.com.alura.forum.modelo.Curso;
  * Para testes com classes repository essas anotações precisam estar presentes:
  * @DataJpaTest - A anotação simplifica a escrita de testes automatizados de interfaces Repository.
  * @RunWith(SpringRunner.class) - A anotação serve para que o Spring inicialize o servidor e carregue os beans da aplicação durante a execução dos testes automatizados.
+ * @AutoConfigureTestDatabase(replace = Replace.NONE) - Utilizar essa anotação para que o Spring não considere que os testes devem sempre ser executados em um banco de dados em memória.
+ * @ActiveProfiles("test") - Força carregar o profile de test e o spring vai buscar as configurações do banco no application-test.properties.
  */
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ActiveProfiles("test")
 public class CursoRepositoryTest {
 
 	/*
@@ -26,10 +34,25 @@ public class CursoRepositoryTest {
 	@Autowired
 	private CursoRepository cursoRepository;
 
+	/*
+	 * Criando uma instância do EntityManager no ambiente de teste para persistir
+	 * dados em um banco de dados de teste vazio
+	 */
+
+	@Autowired
+	private TestEntityManager testManager;
+
 	@Test // pacote org.junit.Test
 	@DisplayName("Deve retornar um curso ao buscar pelo seu nome")
 	public void test() {
 		String nomeCurso = "HTML 5";
+
+		// Criando um curso e persistindo no banco de dados de teste
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programação");
+		testManager.persist(html5);
+
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 
 		Assert.assertNotNull(curso);
