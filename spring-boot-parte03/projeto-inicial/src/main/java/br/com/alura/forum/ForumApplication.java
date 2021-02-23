9ee01b228a5b799a7c17daf1fe7746f3963c71bf
Celso Ribeiro <celsofilho82@gmail.com>
2021-02-22 21:40:43 -0300

    Gerando uma imagem docker do projeto

--- spring-boot-parte03/projeto-inicial/src/main/java/br/com/alura/forum/ForumApplication.java
+++ spring-boot-parte03/projeto-inicial/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,6 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -13,7 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication {
 
 	/*
 	 * Para gerar um WAR da aplicação para ser executado em um sevlet container
@@ -27,11 +25,6 @@ public class ForumApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
 	}
 
 }
