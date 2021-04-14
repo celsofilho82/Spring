6046f35946f29cae91e68c6daa475cc6da611d96
Celso Ribeiro <celsofilho82@gmail.com>
2021-04-13 21:45:07 -0300

    Refatorando o código criando a classe DAO

--- JPA-Introducao-Hibernate/jpa-hibernate-persistencia/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ JPA-Introducao-Hibernate/jpa-hibernate-persistencia/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -3,10 +3,10 @@ package br.com.alura.loja.testes;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
@@ -15,13 +15,13 @@ public class CadastroDeProduto {
 		celular.setNome("Xiaomi Redmi");
 		celular.setDescricao("Muito bom!");
 		celular.setPreco(BigDecimal.valueOf(800));
-		
-		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("loja");
-		EntityManager em = factory.createEntityManager();
-		
+
+		EntityManager em = JPAUtil.getEntityManager();
+
+		ProdutoDao dao = new ProdutoDao(em);
+
 		em.getTransaction().begin();
-		em.persist(celular);
+		dao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
 	}
